1ede2d64827784bd7ea361b46ca28113876f8bd4
iamdurlav <durlavbose@gmail.com>
2021-10-06 23:12:33 +0600

    updated

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,6 +19,22 @@ export default {
 <style lang="css">
 a {
   text-decoration: none;
+  color: rgb(196, 112, 3) !important;
+  position: relative !important;
+}
+
+a::before {
+  content: "";
+  position: absolute;
+  width: 40%;
+  height: 2px;
+  background-color: burlywood;
+  bottom: 25px;
+  left: 17px;
+}
+
+a:hover {
+  color: rgb(212, 145, 82) !important;
 }
 
 h1 {
@@ -50,7 +66,7 @@ h1 {
 }
 
 .uk-navbar-container {
-  background: rgba(255, 243, 236, 0.897) !important;
+  background: rgba(255, 255, 255, 0.897) !important;
   font-family: Staatliches;
 }
 
